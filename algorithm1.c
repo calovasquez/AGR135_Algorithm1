8b420a76de406f89b6e405b860b23c2d999f41a4
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 00:31:34 -0600

    Added algorithm1 function

--- algorithm1.c
+++ algorithm1.c
@@ -20,3 +20,50 @@ void draw(void){
     glFlush();
 }
 
+// Function to draw my carnet
+void algorithm1(float x1, float y1, float x2, float y2){
+    float m, b;
+    m = (y2 - y1) / (x2 - x1);
+    b = y1 - (m * x1);
+    // If the line is vertical:
+    if (x1 == x2) {
+        // Draw a vertical line
+        int startY = (y1 < y2) ? y1 : y2;
+        int endY = (y1 > y2) ? y1 : y2;
+        for (int j = startY; j <= endY; j++) {
+            glBegin(GL_POINTS);
+            glVertex2f(x1, j);
+            glEnd();
+        }
+    } 
+    /* If the point goes 
+     * from left to right and from down to up
+     * or from left to right and from up to down:
+     */
+    else {
+        if ((x2 > x1 && y2 > y1) || (x2 > x1 && y2 < y1)) {
+            for (int i = x1; i <= x2; i++) {
+                float y = (m * i) + b;
+                glBegin(GL_POINTS);
+                glVertex2f(i, y);
+                glEnd();
+            }
+        } 
+        // If not:
+        else {
+            for (int i = x1; i >= x2; i--) {
+                float y = (m * i) + b;
+                glBegin(GL_POINTS);
+                glVertex2f(i, y);
+                glEnd();
+            }
+        }
+    }
+    // And if it is neither of the cases:
+    for (int i = x1; i <= x2; i++){
+            float y = (m * i) + b;
+            glBegin(GL_POINTS);
+            glVertex2f(i, y);
+            glEnd();
+        }
+}
