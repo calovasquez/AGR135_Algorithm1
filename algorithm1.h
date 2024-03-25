7bb2972578c74fbffe04620a8aabce40787f866f
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 00:22:28 -0600

    Added algorithm1.h

---
+++ algorithm1.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   algorithm1.h
+ * Author: vasquez
+ *
+ * Created on March 25, 2024, 12:21 AM
+ */
+
+#ifndef ALGORITHM1_H
+#define ALGORITHM1_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ALGORITHM1_H */
