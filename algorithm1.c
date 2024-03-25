eb409283c4e09eb35cf60d0b74753935af72e0ff
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 00:39:05 -0600

    Added carnet function

--- algorithm1.c
+++ algorithm1.c
@@ -67,3 +67,52 @@ void algorithm1(float x1, float y1, float x2, float y2){
             glEnd();
         }
 }
+
+void carnet(){
+    glColor3i(0, 0, 0); // Black color
+    
+    // Coordinates to draw my carnet
+    
+        // Letter V
+        algorithm1(20, 440, 80, 260);
+        algorithm1(80, 260, 140, 440);
+
+        // Letter G
+        algorithm1(260, 440, 160, 440);
+        algorithm1(160, 440, 160, 260);
+        algorithm1(160, 260, 260, 260);
+        algorithm1(260, 260, 260, 350);
+        algorithm1(260, 350, 210, 350);
+        algorithm1(210, 350, 210, 320);
+
+        // Number 2
+        algorithm1(280, 420, 280, 440);
+        algorithm1(280, 440, 360, 440);
+        algorithm1(360, 440, 360, 350);
+        algorithm1(360, 350, 280, 350);
+        algorithm1(280, 350, 280, 260);
+        algorithm1(280, 260, 360, 260);
+        algorithm1(360, 260, 360, 280);
+
+        // Number 1
+        algorithm1(380, 380, 430, 440);
+        algorithm1(430, 440, 430, 260);
+
+        // Number 0
+        algorithm1(510, 440, 450, 440);
+        algorithm1(450, 440, 450, 260);
+        algorithm1(450, 260, 510, 260);
+        algorithm1(510, 260, 510, 440);
+
+        // Number 0
+        algorithm1(590, 440, 530, 440);
+        algorithm1(530, 440, 530, 260);
+        algorithm1(530, 260, 590, 260);
+        algorithm1(590, 260, 590, 440);
+
+        // Number 9
+        algorithm1(680, 260, 680, 440);
+        algorithm1(680, 440, 610, 440);
+        algorithm1(610, 440, 610, 360);
+        algorithm1(610, 360, 680, 360);
+}
