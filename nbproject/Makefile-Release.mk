5f0e4f694e7ee02bfd0c9f0ae5457f5031b585d9
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 00:21:41 -0600

    Added algorithm1.c

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/algorithm1.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/agr135_algorithm1: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/agr135_algorithm1 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/algorithm1.o: algorithm1.c
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/algorithm1.o algorithm1.c
 
 ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
