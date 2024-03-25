cac548cc7c4d28feeda288faacc840fa0684a35b
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 00:49:46 -0600

    Updated project

---
+++ build/Debug/GNU-Linux/algorithm1.o.d
@@ -0,0 +1,2 @@
+build/Debug/GNU-Linux/algorithm1.o: algorithm1.c algorithm1.h
+algorithm1.h:
--- build/Debug/GNU-Linux/main.o.d
+++ build/Debug/GNU-Linux/main.o.d
@@ -1,1 +1,2 @@
-build/Debug/GNU-Linux/main.o: main.c
+build/Debug/GNU-Linux/main.o: main.c algorithm1.h
+algorithm1.h:
